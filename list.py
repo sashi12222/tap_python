a049b1d960222adf1bfddd8c7418e01bb1337bac
sashi12222 <sashiraj1222@gmail.com>
2023-12-01 16:01:17 +0530

    hello

--- list.py
+++ list.py
@@ -96,7 +96,7 @@ NOTE:
          n=[10,20,30,40]
          print(len(n))
     2.index():Returns the  index of first occurence of the specified item.
-        ex:
+        ex: 
         n=[1,2,2,2,2,3,3]
         print(n.index(1))
         print(n.index(2))
